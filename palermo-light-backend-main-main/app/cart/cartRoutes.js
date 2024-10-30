c44a1ed8769852ab21f94cfb0a0172e26df1a16f
Qwerty <houldcould78@mail.ru>
2024-10-30 18:46:42 +0300

    one

--- palermo-light-backend-main-main/app/cart/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getCartProducts } = require('./cartControllers'); 
-
-const router = express.Router();
-
-router.post('/cart/products', getCartProducts);
-
-module.exports = router;
